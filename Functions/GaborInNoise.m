b91c5369653c5ce49b29732630cd2648f61c54e0
AllenMuhanChen <achen52@u.rochester.edu>
2017-04-07 15:43:37 -0400

    Delete GaborInNoise.m

--- Functions/GaborInNoise.m
+++
@@ -1,7 +0,0 @@
-function stimulusMatrix = GaborInNoise([Gabor_Diameter, Sigma, Lambda, Orientation, Phase, Amplitude],[Type,SideLengthX, SideLengthY, Color, BackgroundColor])
-%Creates pixel value matrix of a Gabor embedded into noise and an apperature
-%   Detailed explanation goes here
-
-
-end
-
