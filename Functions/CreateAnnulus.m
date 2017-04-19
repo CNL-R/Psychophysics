1871dd5122c6198fc2f283baaf638e8e5108da96
AllenMuhanChen <achen52@u.rochester.edu>
2017-04-19 12:36:42 -0400

    More Functions

---
+++ Functions/CreateAnnulus.m
@@ -0,0 +1,25 @@
+function annulusMatrix = CreateAnnulus(Diameter, AnnulusWidth, AnnulusColor, BackgroundColor)
+
+%Drawing annulus. 
+annulusMatrix = repmat(BackgroundColor, Diameter, Diameter);
+
+%Drawing Outer Circle
+for y = 1:Diameter
+    for x = 1:Diameter
+        if ((x - Diameter/2)^2 + (y - Diameter/2)^2) < (Diameter/2)^2
+            annulusMatrix(y,x) = AnnulusColor;
+        end
+    end 
+end 
+
+%Drawing Inner Circle
+for y = 1:Diameter
+    for x = 1:Diameter
+         if ((x - Diameter/2)^2 + (y - Diameter/2)^2) < ((Diameter/2) - AnnulusWidth)^2
+            annulusMatrix(y,x) = BackgroundColor;
+        end
+    end 
+end 
+end
+
+
---
+++ Functions/PresentBeep.m
@@ -0,0 +1,17 @@
+function [] = PresentBeep(Frequency, Duration, NumberChannels, Volume)
+%given a frequency, duration, and number of channels, play beep. Duration
+%is in ms and volume is 0 - 1. 
+    Duration = Duration / 1000;
+    sampleFreq = 48000;
+    repetitions = 1;
+    startCue = 0;
+    waitForDeviceStart = 1;
+    pahandle = PsychPortAudio('Open', [], 1, 1, sampleFreq, NumberChannels, [], [], [], []);
+    PsychPortAudio('Volume', pahandle, Volume);
+    myBeep = MakeBeep(Frequency, Duration, sampleFreq);
+    PsychPortAudio('FillBuffer', pahandle, [myBeep; myBeep]);
+    PsychPortAudio('Start', pahandle, repetitions, startCue, waitForDeviceStart);
+    PsychPortAudio('Stop', pahandle, 1, 1);
+    PsychPortAudio('Close', pahandle);
+end
+
