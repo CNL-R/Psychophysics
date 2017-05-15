d94de88e7936fff114eb037b76e308fb9a7e8953
Allen Chen <achen52@u.rochester.edu>
2017-05-15 16:52:34 -0400

    Info for Kamy's Friend

--- Prototype Code/SerialCodes.m
+++ Prototype Code/SerialCodes.m
@@ -1,4 +1,5 @@
-%address = hex2dec('C010');
+address = hex2dec('C010'); %Address is the first number of the I/O Range of a COM Port. It is given as a Hexadecimal number but must be converted to decimal.
+config_io; %Needs to be run once in the beggining of the script before outp or inp can be used
 
 outp(address, 3)
 pause(0.005)
